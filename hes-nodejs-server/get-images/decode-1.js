c4067dd64e8659408edc828506e7a95bc6c717fe
johnvincentio <john@johnvincent.io>
2017-05-04 13:03:27 -0400

    clean

--- hes-nodejs-server/get-images/decode-1.js
+++
@@ -1,4 +0,0 @@
-var jv = decodeURI("https://developer.mozilla.org/ru/docs/JavaScript_%D1%88%D0%B5%D0%BB%D0%BB%D1%8B");
-// "https://developer.mozilla.org/ru/docs/JavaScript_шеллы"
-
-console.log("jv :"+jv+":");
